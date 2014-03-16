e24c5152d48a9426eaf122ef2189c676fe7952e3
Eduardo Poiate <eduardopoiate@gmail.com>
2014-03-16 18:08:54 -0300

    botões implementados, falta excessoes

--- Yu-Gi-OhLifePointsCalculator/src/com/epoiate/yugiohLPC/MainActivity.java
+++ Yu-Gi-OhLifePointsCalculator/src/com/epoiate/yugiohLPC/MainActivity.java
@@ -21,9 +21,14 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_game);
-//      lifePoint1.setText(Integer.toString(this.lp1));
-//      lifePoint2.setText(Integer.toString(this.lp2));
-		setUpGUI();
+	
+		damage1 = (EditText) findViewById(R.id.p1_damage);
+		damage2 = (EditText) findViewById(R.id.p2_damage);
+	    lifePoint1 = (TextView) findViewById(R.id.p1_LP);
+		lifePoint2 = (TextView) findViewById(R.id.p2_LP);
+        lifePoint1.setText(Integer.toString(lp1));
+        lifePoint2.setText(Integer.toString(lp2));
+	
 	}
 
 
@@ -36,84 +41,163 @@ public class MainActivity extends Activity {
         public static final int exit_game=0x7f090002;
         public static final int load_game=0x7f090001;
         public static final int new_game=0x7f090000;
+		
+		case R.id.p1_LP:       //0x7f090003
+	    case R.id.p1_damage:   //0x7f090006
+		case R.id.p1_progress: //0x7f090004
+		case R.id.p2_LP:       //0x7f09001e
+		case R.id.p2_damage:   //0x7f09001b
+		case R.id.p2_progress: //0x7f09001d
         */
 		default: 
-        case R.id.p1_LP:       //0x7f090003
-        	
-        case R.id.p1_damage:   //0x7f090006
+			break;
+        
         case R.id.p1_double:   //0x7f09000f
-				    
+			lp1 = 2*lp1;
+			lifePoint1.setText(Integer.toString(lp1));
+			break;
+			
         case R.id.p1_gain:     //0x7f090007
-			//try{
-				lp1 += Integer.parseInt(lifePoint1.getText().toString());
+			try{
+				lp1 += Integer.parseInt(damage1.getText().toString());
 				lifePoint1.setText(Integer.toString(lp1));
 				break;
-			//}catch (Exception e){System.out.println(e);}
+			}catch (Exception e){}
+			
         case R.id.p1_gain_100: //0x7f09000a
+			try{
+				lp1 += 100;
+				lifePoint1.setText(Integer.toString(lp1));
+				break;
+			}catch (Exception e){}
+	
         case R.id.p1_gain_1000://0x7f090010
+			try{
+				lp1 += 1000;
+				lifePoint1.setText(Integer.toString(lp1));
+				break;
+			}catch (Exception e){}
+					
         case R.id.p1_gain_500: //0x7f09000d
-        case R.id.p1_half:     //0x7f090=00c
+			try{
+				lp1 += 500;
+				lifePoint1.setText(Integer.toString(lp1));
+				break;
+			}catch (Exception e){}
+					
+		
+        case R.id.p1_half:     //0x7f09000c
+			lp1 = lp1/2;
+			lifePoint1.setText(Integer.toString(lp1));
+			break;
+			
         case R.id.p1_lose:     //0x7f090005
+			try{
+				lp1 -= Integer.parseInt(damage1.getText().toString());
+				lifePoint1.setText(Integer.toString(lp1));
+				break;
+			}catch (Exception e){}
+			
         case R.id.p1_lose_100: //0x7f090008
+		try{
+				lp1 -= 100;
+				lifePoint1.setText(Integer.toString(lp1));
+				break;
+			}catch (Exception e){}
+					
         case R.id.p1_lose_1000://0x7f09000e
+			try{
+				lp1 -= 1000;
+				lifePoint1.setText(Integer.toString(lp1));
+				break;
+			}catch (Exception e){}
+					
         case R.id.p1_lose_500: //0x7f09000b
-        case R.id.p1_progress: //0x7f090004
+			try{
+				lp1 -= 500;
+				lifePoint1.setText(Integer.toString(lp1));
+				break;
+			}catch (Exception e){}
+					
+
         case R.id.p1_reset:    //0x7f090009
+			lp1 = 8000;
+			lifePoint1.setText(Integer.toString(lp1));
+			break;
+			
         ///////////////////////////////////
-        case R.id.p2_LP:       //0x7f09001e
-        case R.id.p2_damage:   //0x7f09001b
-        case R.id.p2_double:   //0x7f090012
-        case R.id.p2_gain:     //0x7f09001a
-        case R.id.p2_gain_100: //0x7f090017
-        case R.id.p2_gain_1000://0x7f090011
-        case R.id.p2_gain_500: //0x7f090014
-        case R.id.p2_half:     //0x7f090015
-        case R.id.p2_lose:     //0x7f09001c
-        case R.id.p2_lose_100: //0x7f090019
-        case R.id.p2_lose_1000://0x7f090013
-        case R.id.p2_lose_500: //0x7f090016
-        case R.id.p2_progress: //0x7f09001d
-        case R.id.p2_reset:    //0x7f090018
         
+        case R.id.p2_double:   //0x7f090012
+			lp2 = 2*lp2;
+			lifePoint2.setText(Integer.toString(lp2));
+			break;
+			
+        case R.id.p2_gain:     //0x7f09001a
+			try{
+				lp2 += Integer.parseInt(damage2.getText().toString());
+				lifePoint2.setText(Integer.toString(lp2));
+				break;
+			}catch (Exception e){}
+			
+        case R.id.p2_gain_100: //0x7f090017
+			try{
+				lp2 += 100;
+				lifePoint2.setText(Integer.toString(lp2));
+				break;
+			}catch (Exception e){}
+					
+        case R.id.p2_gain_1000://0x7f090011
+			try{
+				lp2 += 1000;
+				lifePoint2.setText(Integer.toString(lp2));
+				break;
+			}catch (Exception e){}
+	
+        case R.id.p2_gain_500: //0x7f090014
+			try{
+				lp2 += 500;
+				lifePoint2.setText(Integer.toString(lp2));
+				break;
+			}catch (Exception e){}
+					
+        case R.id.p2_half:     //0x7f090015
+			lp2 = lp2/2;
+			lifePoint2.setText(Integer.toString(lp2));
+			break;
+			
+		case R.id.p2_lose:     //0x7f09001c
+			try{
+				lp2 -= Integer.parseInt(damage2.getText().toString());
+				lifePoint2.setText(Integer.toString(lp2));
+				break;
+			}catch (Exception e){}
+			
+        case R.id.p2_lose_100: //0x7f090019
+			try{
+				lp2 -= 100;
+				lifePoint2.setText(Integer.toString(lp2));
+				break;
+			}catch (Exception e){}
+			
+        case R.id.p2_lose_1000://0x7f090013
+			try{
+				lp2 -= 1000;
+				lifePoint2.setText(Integer.toString(lp2));
+				break;
+			}catch (Exception e){}
+			
+        case R.id.p2_lose_500: //0x7f090016
+			try{
+				lp2 -= 500;
+				lifePoint2.setText(Integer.toString(lp2));
+				break;
+			}catch (Exception e){}
+        
+        case R.id.p2_reset:    //0x7f090018
+			lp2 = 8000;
+			lifePoint2.setText(Integer.toString(lp2));
+        	break;
 		}
 
 	}
-
-	public void setUpGUI()
-	  {
-/*
-	    this.LifePoint1 = ((EditTextView)findViewById(R.id.p1_LP));
-	    this.LifePoint2 = ((EditTextView)findViewById(R.id.p2_LP));
-	    this.LifePoint1.setOnTextChangeListener(this);
-	    this.LifePoint2.setOnTextChangeListener(this);
-	    this.LifePoint1.setEnabled(false);
-	    this.LifePoint2.setEnabled(false);
-	    this.damage1 = ((EditText)findViewById(R.id.p1_damage));
-	    this.damage2 = ((EditText)findViewById(R.id.p2_damage));
-	    
-	    findViewById(R.id.p1_double).setOnClickListener(this);
-	    findViewById(R.id.p1_gain).setOnClickListener(this);
-	    findViewById(R.id.p1_gain_100).setOnClickListener(this);
-	    findViewById(R.id.p1_gain_1000).setOnClickListener(this);
-	    findViewById(R.id.p1_gain_500).setOnClickListener(this);
-	    findViewById(R.id.p1_half).setOnClickListener(this);
-	    findViewById(R.id.p1_lose).setOnClickListener(this);
-	    findViewById(R.id.p1_lose_100).setOnClickListener(this);
-	    findViewById(R.id.p1_lose_1000).setOnClickListener(this);
-	    findViewById(R.id.p1_lose_500).setOnClickListener(this);
-	    findViewById(R.id.p1_reset).setOnClickListener(this);
-	    
-	    findViewById(R.id.p2_double).setOnClickListener(this);
-	    findViewById(R.id.p2_gain).setOnClickListener(this);
-	    findViewById(R.id.p2_gain_100).setOnClickListener(this);
-	    findViewById(R.id.p2_gain_1000).setOnClickListener(this);
-	    findViewById(R.id.p2_gain_500).setOnClickListener(this);
-	    findViewById(R.id.p2_half).setOnClickListener(this);
-	    findViewById(R.id.p2_lose).setOnClickListener(this);
-	    findViewById(R.id.p2_lose_100).setOnClickListener(this);
-	    findViewById(R.id.p2_lose_1000).setOnClickListener(this);
-	    findViewById(R.id.p2_lose_500).setOnClickListener(this);
-	    findViewById(R.id.p2_reset).setOnClickListener(this);
-	    */
-	  }
 }
